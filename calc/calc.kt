841e241acb57eff4a2b8bd27e5485577baa4fde5
janupponi <76932601+janupponi@users.noreply.github.com>
2023-01-03 08:07:43 +0530

    Update calc.kt

--- calc/calc.kt
+++ calc/calc.kt
@@ -110,3 +110,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
+
+
+
+
+//dependencies check
+dependencies {
+
+    implementation 'androidx.core:core-ktx:1.7.0'
+    implementation 'androidx.appcompat:appcompat:1.5.1'
+    implementation 'com.google.android.material:material:1.7.0'
+    implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
+    implementation 'org.mariuszgromada.math:MathParser.org-mXparser:4.4.2'
+    testImplementation 'junit:junit:4.13.2'
+    androidTestImplementation 'androidx.test.ext:junit:1.1.4'
+    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.0'
+}
