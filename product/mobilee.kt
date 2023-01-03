beded05fa87c1e486b140cb2e9f27fb3fda4ef59
janupponi <76932601+janupponi@users.noreply.github.com>
2023-01-03 07:46:14 +0530

    Create mobilee.kt

---
+++ product/mobilee.kt
@@ -0,0 +1,6 @@
+class mobilee : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mobilee)
+    }
+}
