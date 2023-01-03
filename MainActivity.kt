8dffd094e0b2f1a4e7bf5aea831c76c1c463cb66
janupponi <76932601+janupponi@users.noreply.github.com>
2023-01-03 07:04:24 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,37 @@
+package college.edu.practical3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+
+
+class MainActivity : AppCompatActivity() {
+    var outcome=""
+    fun decide(){
+        val randomnum = (Random.nextInt(0, 8))
+        when (randomnum){
+            0->outcome="Yes, Go for it!!"
+            1->outcome="No, leave it."
+            2->outcome="Maybe..."
+            3->outcome="Ask again later?"
+            4->outcome="Most Probably."
+            5->outcome="Think about it again..."
+            6->outcome="ABSOLUTELY NOT"
+            7->outcome="YES! YES! YES!"
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val Output = findViewById<TextView>(R.id.output)
+        val Decide = findViewById<Button>(R.id.button)
+
+        Decide.setOnClickListener{
+            decide()
+            Output.text=outcome
+        }
+    }
+}
